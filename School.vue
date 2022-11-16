b07cdb3c1fbdc799394b7339146d76b5424114ab
李拜五 <11729673+ll2524894146@user.noreply.gitee.com>
2022-11-16 15:40:48 +0800

    提交

---
+++ School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+        name:'尚硅谷',
+        address:'北京平昌'
+    }
+},
+methods: {
+    showName(){
+        alert(this.showName)
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
---
+++ main.js
@@ -0,0 +1,5 @@
+import App from './App.Vue'
+new vue({
+    el:'#app',
+    component:{App}
+})
